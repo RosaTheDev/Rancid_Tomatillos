811d5d4b62ae82d7d81db3234e1ced96fa3f8d75
Richard Rosa-Serrano <richardjrosaserrano@gmail.com>
2022-02-04 20:42:20 -0500

    Working on integrating the API without having refresh lose all movie covers

--- src/App/App.js
+++ src/App/App.js
@@ -8,15 +8,25 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      error: ''
+      movies: [],
+      error: false,
     }
   }
+  
+    componentDidMount() {
+     fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
+      .then(response => response.json())
+      .then(movies => this.setState({movies}))
+      .catch(() => this.setState({error: true}))
+      console.log('I mounted 1st')
+    }
+  
 
   render() {
     return (
       <section >
         <Nav />
-        <Main />
+        <Main moviesProps={this.state.movies}/>
       </section>
     )
   }
--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,14 +1,26 @@
 import React from 'react'
 import './Main.css'
+import Movie from './Movie/Movie'
 
-const Main = () => {
+const Main = ({moviesProps}) => {
+  // const  movies  = moviesProps
+  console.log('main container', moviesProps.movies)
+  // setTimeout(console.log(movies), 3000)
+  
+    const movieCards = moviesProps.movies.map(movie => {
+      return (
+      <Movie 
+        title={movie.title}
+        poster={movie.poster_path}
+        avgRating={movie.average_rating}
+        />
+      )
+    })
   return (
     <main>
       <h1 className="Category-title">Upcoming Movies</h1>
       <section className='movies-section'>
-        <article className='movie'>
-          
-        </article>
+         {movieCards}
       </section>
     </main>
   )
